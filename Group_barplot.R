bae301a2c0d0c1212d7273cdceb676d15eeedc61
Dong Min Lim <dongmin7591@gmail.com>
2022-12-14 16:38:23 +0900

    .

---
+++ Group_barplot.R
@@ -0,0 +1,32 @@
+bp<-read.csv(file = "./InterCellDB/Barplot/6955.csv")
+
+#1. 임상정보 High, Low 두개의 데이터프레임 만들기
+q<-which(bp$sending%in%"Non classical monocytes")
+p<-which(bp$sending%in%"Myeloid dendritic cells")
+
+ncMonocyte<-bp[q,]
+mDC<-bp[p,]
+
+colnames(ncMonocyte)
+
+# Change the colors manually
+p <- ggplot(data=ncMonocyte, aes(x=receiving, y=power, fill=Group)) +
+  geom_bar(stat="identity", color="black", position=position_dodge())+
+  theme_minimal()
+# Use custom colors
+p + scale_fill_manual(values=c('#999999','#E69F00'))+ theme(axis.text.x = element_text(angle = 90))
+
+
+# Change the colors manually
+p <- ggplot(data=ncMonocyte, aes(x=receiving, y=power, fill=Group)) +
+  geom_bar(stat="identity", color="black", position=position_dodge())+
+  theme_cowplot()+labs(title="Non classical monocytes (sending cells)", x="signal receiving cells", y = "Power")+
+  theme(
+    plot.title = element_text(size=14, face="bold"),
+    axis.title.x = element_text(size=14),
+    axis.title.y = element_text( size=14))+theme(axis.text.x = element_text(size=12))
+# Use custom colors
+p + scale_fill_manual(values=c('#999999','#E69F00'))+ theme(axis.text.x = element_text(angle = 90))
+
+png("./InterCellDB/Healthy/GO:0006955/ncMonocyte_Power_barplot.png",width=4000,height=4000,res=500)
+dev.off()
